aadf67fdca0d515fc3d84130151606855d7dcecf
Sergei Rozhkov <fgup@zserg.ru>
2025-03-12 17:50:04 +0500

    Update

--- .shell/zprint.sh
+++ .shell/zprint.sh
@@ -11,7 +11,7 @@ if [ $# -ne 2 ]; then echo "Используйте $0 PRINT|CLOSE FILE"; exit 1;
 if [ -f /ZMOD ]; then
     CCURL="/usr/bin/curl"
 else
-    if [ ${FF5X} -eq 1 ] then
+    if [ ${FF5X} -eq 1 ]; then
         export LD_LIBRARY_PATH=//usr/prog/qt-4.8.6/lib:$LD_LIBRARY_PATH
         export LD_LIBRARY_PATH=/usr/prog/openssl-1.0.2d/lib:$LD_LIBRARY_PATH
         export LD_LIBRARY_PATH=/usr/prog/curl-7.55.1-https/lib:$LD_LIBRARY_PATH
