0161e2a4cd26192c6c35efaf68d97847803dd904
Sergei Rozhkov <fgup@zserg.ru>
2025-03-14 14:34:58 +0500

    CHECK_LINK

--- .shell/md5.sh
+++ .shell/md5.sh
@@ -13,39 +13,25 @@ find .  \
     -and -not -name "md5.sh" \
     -and -not -name "link.sh" \
     -and -not -name "list.link" \
-    -and -not -path "./.rnd" \
-    -and -not -path "./.wpa_cli_history" \
-    -and -not -path "./root/printer_data/version.txt" \
-    -and -not -path "./etc/dropbear/*" \
-    -and -not -path "./etc/timezone" \
-    -and -not -path "./etc/localtime" \
-    -and -not -path "./opt/config/*" \
-    -and -not -path "./opt/PROGRAM/control/*" \
-    -and -not -path "./opt/PROGRAM/kernel/*" \
-    -and -not -path "./opt/PROGRAM/library/*" \
-    -and -not -path "./opt/PROGRAM/software/*" \
+    -and -not -path "./usr/data/config/*" \
+    -and -not -path "./usr/prog/PROGRAM/control/*" \
+    -and -not -path "./usr/prog/PROGRAM/kernel/*" \
+    -and -not -path "./usr/prog/PROGRAM/library/*" \
+    -and -not -path "./usr/prog/PROGRAM/software/*" \
     -and -not -path "./opt/auto_run.sh" \
-    -and -not -path "./root/version" \
-    -and -not -path "./root/.viminfo" \
-    -and -not -path "./opt/klipper/klippy/extras/spi_temperature.py" \
-    -and -not -path "./opt/klipper/klippy/extras/gcode_shell_command.py" \
-    -and -not -path "./opt/klipper/klippy/extras/buttons.py" \
-    -and -not -path "./opt/klipper/klippy/extras/save_variables.py" \
-    -and -not -path "./opt/klipper/klippy/mcu.py" \
-    -and -not -path "./opt/klipper/klippy/toolhead.py" \
-    -and -not -path "./opt/klipper/klippy/webhooks.py" \
-    -and -not -path "./opt/key.priv" \
-    -and -not -path "./opt/private.pem" \
-    -and -not -path "./opt/key.pub" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/spi_temperature.py" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/gcode_shell_command.py" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/buttons.py" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/save_variables.py" \
+    -and -not -path "./usr/prog/klipper/klippy/mcu.py" \
+    -and -not -path "./usr/prog/klipper/klippy/toolhead.py" \
+    -and -not -path "./usr/prog/klipper/klippy/webhooks.py" \
     -and -not -path "./etc/shadow-" \
     -and -not -path "./etc/shadow" \
-    -and -not -path "./etc/rtl_hostapd_2G.conf" \
-    -and -not -path "./etc/hosts" \
-    -and -not -path "./etc/random-seed" \
-    -and -not -path "./etc/MAC" \
-    -and -not -path "./etc/wpa_supplicant.conf" \
-    -and -not -path "./opt/tslib-1.12/etc/pointercal" \
-    -and -not -path "./Settings/Trolltech.conf" \
+    -and -not -path "./usr/prog/MAC" \
+    -and -not -path "./usr/prog/app_startup.sh" \
+    -and -not -path "./usr/prog/wifi/wpa_supplicant.conf" \
+    -and -not -path "/usr/prog/tslib-1.12/etc/pointercal" \
     -exec mkdir -p ../stock/{} \;
 
 echo "#!/bin/sh
@@ -80,51 +66,6 @@ check_file()
 
 " >list.link
 
-echo "echo 'Проверка прав на каталоги...'">>list.link
-find .  \
-    -type d \
-    -and -not -name "*.pyc" \
-    -and -not -path "./dev*" \
-    -and -not -path "./run*" \
-    -and -not -path "*__pycache__*" \
-    -and -not -name "md5sum.list" \
-    -and -not -name "md5.sh" \
-    -and -not -name "link.sh" \
-    -and -not -name "list.link" \
-    -and -not -path "./.rnd" \
-    -and -not -path "./.wpa_cli_history" \
-    -and -not -path "./root/printer_data/version.txt" \
-    -and -not -path "./etc/dropbear/*" \
-    -and -not -path "./etc/timezone" \
-    -and -not -path "./etc/localtime" \
-    -and -not -path "./opt/config/*" \
-    -and -not -path "./opt/PROGRAM/control/*" \
-    -and -not -path "./opt/PROGRAM/kernel/*" \
-    -and -not -path "./opt/PROGRAM/library/*" \
-    -and -not -path "./opt/PROGRAM/software/*" \
-    -and -not -path "./opt/auto_run.sh" \
-    -and -not -path "./root/version" \
-    -and -not -path "./root/.viminfo" \
-    -and -not -path "./opt/klipper/klippy/extras/spi_temperature.py" \
-    -and -not -path "./opt/klipper/klippy/extras/gcode_shell_command.py" \
-    -and -not -path "./opt/klipper/klippy/extras/buttons.py" \
-    -and -not -path "./opt/klipper/klippy/extras/save_variables.py" \
-    -and -not -path "./opt/klipper/klippy/mcu.py" \
-    -and -not -path "./opt/klipper/klippy/toolhead.py" \
-    -and -not -path "./opt/klipper/klippy/webhooks.py" \
-    -and -not -path "./opt/key.priv" \
-    -and -not -path "./opt/private.pem" \
-    -and -not -path "./opt/key.pub" \
-    -and -not -path "./etc/shadow-" \
-    -and -not -path "./etc/shadow" \
-    -and -not -path "./etc/rtl_hostapd_2G.conf" \
-    -and -not -path "./etc/hosts" \
-    -and -not -path "./etc/random-seed" \
-    -and -not -path "./etc/MAC" \
-    -and -not -path "./etc/wpa_supplicant.conf" \
-    -and -not -path "./opt/tslib-1.12/etc/pointercal" \
-    -and -not -path "./Settings/Trolltech.conf" \
-    -exec ./link.sh {} "dir" \; >>list.link
 
 echo "echo 'Проверка символических ссылок...'" >>list.link
 chmod +x list.link
@@ -138,42 +79,27 @@ find .  \
     -and -not -name "md5.sh" \
     -and -not -name "link.sh" \
     -and -not -name "list.link" \
-    -and -not -path "./.rnd" \
-    -and -not -path "./.wpa_cli_history" \
-    -and -not -path "./root/printer_data/version.txt" \
-    -and -not -path "./etc/dropbear/*" \
-    -and -not -path "./etc/timezone" \
-    -and -not -path "./etc/localtime" \
-    -and -not -path "./opt/config/*" \
-    -and -not -path "./opt/PROGRAM/control/*" \
-    -and -not -path "./opt/PROGRAM/kernel/*" \
-    -and -not -path "./opt/PROGRAM/library/*" \
-    -and -not -path "./opt/PROGRAM/software/*" \
+    -and -not -path "./usr/data/config/*" \
+    -and -not -path "./usr/prog/PROGRAM/control/*" \
+    -and -not -path "./usr/prog/PROGRAM/kernel/*" \
+    -and -not -path "./usr/prog/PROGRAM/library/*" \
+    -and -not -path "./usr/prog/PROGRAM/software/*" \
     -and -not -path "./opt/auto_run.sh" \
-    -and -not -path "./root/version" \
-    -and -not -path "./root/.viminfo" \
-    -and -not -path "./opt/klipper/klippy/extras/spi_temperature.py" \
-    -and -not -path "./opt/klipper/klippy/extras/gcode_shell_command.py" \
-    -and -not -path "./opt/klipper/klippy/extras/buttons.py" \
-    -and -not -path "./opt/klipper/klippy/extras/save_variables.py" \
-    -and -not -path "./opt/klipper/klippy/mcu.py" \
-    -and -not -path "./opt/klipper/klippy/toolhead.py" \
-    -and -not -path "./opt/klipper/klippy/webhooks.py" \
-    -and -not -path "./opt/key.priv" \
-    -and -not -path "./opt/private.pem" \
-    -and -not -path "./opt/key.pub" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/spi_temperature.py" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/gcode_shell_command.py" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/buttons.py" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/save_variables.py" \
+    -and -not -path "./usr/prog/klipper/klippy/mcu.py" \
+    -and -not -path "./usr/prog/klipper/klippy/toolhead.py" \
+    -and -not -path "./usr/prog/klipper/klippy/webhooks.py" \
     -and -not -path "./etc/shadow-" \
     -and -not -path "./etc/shadow" \
-    -and -not -path "./etc/rtl_hostapd_2G.conf" \
-    -and -not -path "./etc/hosts" \
-    -and -not -path "./etc/random-seed" \
-    -and -not -path "./etc/MAC" \
-    -and -not -path "./etc/wpa_supplicant.conf" \
-    -and -not -path "./opt/tslib-1.12/etc/pointercal" \
-    -and -not -path "./Settings/Trolltech.conf" \
+    -and -not -path "./usr/prog/MAC" \
+    -and -not -path "./usr/prog/app_startup.sh" \
+    -and -not -path "./usr/prog/wifi/wpa_supplicant.conf" \
+    -and -not -path "/usr/prog/tslib-1.12/etc/pointercal" \
     -exec ./link.sh {} link \; >>list.link
 
-echo "echo 'Проверка прав на файлы...'">>list.link
 find .  \
     -type f \
     -and -not -name "*.pyc" \
@@ -184,84 +110,25 @@ find .  \
     -and -not -name "md5.sh" \
     -and -not -name "link.sh" \
     -and -not -name "list.link" \
-    -and -not -path "./.rnd" \
-    -and -not -path "./.wpa_cli_history" \
-    -and -not -path "./root/printer_data/version.txt" \
-    -and -not -path "./etc/dropbear/*" \
-    -and -not -path "./etc/timezone" \
-    -and -not -path "./etc/localtime" \
-    -and -not -path "./opt/config/*" \
-    -and -not -path "./opt/PROGRAM/control/*" \
-    -and -not -path "./opt/PROGRAM/kernel/*" \
-    -and -not -path "./opt/PROGRAM/library/*" \
-    -and -not -path "./opt/PROGRAM/software/*" \
+    -and -not -path "./usr/data/config/*" \
+    -and -not -path "./usr/prog/PROGRAM/control/*" \
+    -and -not -path "./usr/prog/PROGRAM/kernel/*" \
+    -and -not -path "./usr/prog/PROGRAM/library/*" \
+    -and -not -path "./usr/prog/PROGRAM/software/*" \
     -and -not -path "./opt/auto_run.sh" \
-    -and -not -path "./root/version" \
-    -and -not -path "./root/.viminfo" \
-    -and -not -path "./opt/klipper/klippy/extras/spi_temperature.py" \
-    -and -not -path "./opt/klipper/klippy/extras/gcode_shell_command.py" \
-    -and -not -path "./opt/klipper/klippy/extras/buttons.py" \
-    -and -not -path "./opt/klipper/klippy/extras/save_variables.py" \
-    -and -not -path "./opt/klipper/klippy/mcu.py" \
-    -and -not -path "./opt/klipper/klippy/toolhead.py" \
-    -and -not -path "./opt/klipper/klippy/webhooks.py" \
-    -and -not -path "./opt/key.priv" \
-    -and -not -path "./opt/private.pem" \
-    -and -not -path "./opt/key.pub" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/spi_temperature.py" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/gcode_shell_command.py" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/buttons.py" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/save_variables.py" \
+    -and -not -path "./usr/prog/klipper/klippy/mcu.py" \
+    -and -not -path "./usr/prog/klipper/klippy/toolhead.py" \
+    -and -not -path "./usr/prog/klipper/klippy/webhooks.py" \
     -and -not -path "./etc/shadow-" \
     -and -not -path "./etc/shadow" \
-    -and -not -path "./etc/rtl_hostapd_2G.conf" \
-    -and -not -path "./etc/hosts" \
-    -and -not -path "./etc/random-seed" \
-    -and -not -path "./etc/MAC" \
-    -and -not -path "./etc/wpa_supplicant.conf" \
-    -and -not -path "./opt/tslib-1.12/etc/pointercal" \
-    -and -not -path "./Settings/Trolltech.conf" \
-    -exec ./link.sh {} "file" \; >>list.link
-
-find .  \
-    -type f \
-    -and -not -name "*.pyc" \
-    -and -not -path "./dev*" \
-    -and -not -path "./run*" \
-    -and -not -path "*__pycache__*" \
-    -and -not -name "md5sum.list" \
-    -and -not -name "md5.sh" \
-    -and -not -name "link.sh" \
-    -and -not -name "list.link" \
-    -and -not -path "./.rnd" \
-    -and -not -path "./.wpa_cli_history" \
-    -and -not -path "./root/printer_data/version.txt" \
-    -and -not -path "./etc/dropbear/*" \
-    -and -not -path "./etc/timezone" \
-    -and -not -path "./etc/localtime" \
-    -and -not -path "./opt/config/*" \
-    -and -not -path "./opt/PROGRAM/control/*" \
-    -and -not -path "./opt/PROGRAM/kernel/*" \
-    -and -not -path "./opt/PROGRAM/library/*" \
-    -and -not -path "./opt/PROGRAM/software/*" \
-    -and -not -path "./opt/auto_run.sh" \
-    -and -not -path "./root/version" \
-    -and -not -path "./root/.viminfo" \
-    -and -not -path "./opt/klipper/klippy/extras/spi_temperature.py" \
-    -and -not -path "./opt/klipper/klippy/extras/gcode_shell_command.py" \
-    -and -not -path "./opt/klipper/klippy/extras/buttons.py" \
-    -and -not -path "./opt/klipper/klippy/extras/save_variables.py" \
-    -and -not -path "./opt/klipper/klippy/mcu.py" \
-    -and -not -path "./opt/klipper/klippy/toolhead.py" \
-    -and -not -path "./opt/klipper/klippy/webhooks.py" \
-    -and -not -path "./opt/key.priv" \
-    -and -not -path "./opt/private.pem" \
-    -and -not -path "./opt/key.pub" \
-    -and -not -path "./etc/shadow-" \
-    -and -not -path "./etc/shadow" \
-    -and -not -path "./etc/rtl_hostapd_2G.conf" \
-    -and -not -path "./etc/hosts" \
-    -and -not -path "./etc/random-seed" \
-    -and -not -path "./etc/MAC" \
-    -and -not -path "./etc/wpa_supplicant.conf" \
-    -and -not -path "./opt/tslib-1.12/etc/pointercal" \
-    -and -not -path "./Settings/Trolltech.conf" \
+    -and -not -path "./usr/prog/MAC" \
+    -and -not -path "./usr/prog/app_startup.sh" \
+    -and -not -path "./usr/prog/wifi/wpa_supplicant.conf" \
+    -and -not -path "/usr/prog/tslib-1.12/etc/pointercal" \
     -exec cp -a {} ../stock/{} \;
 
 find .  \
@@ -274,37 +141,88 @@ find .  \
     -and -not -name "md5.sh" \
     -and -not -name "link.sh" \
     -and -not -name "list.link" \
-    -and -not -path "./.rnd" \
-    -and -not -path "./.wpa_cli_history" \
-    -and -not -path "./root/printer_data/version.txt" \
-    -and -not -path "./etc/dropbear/*" \
-    -and -not -path "./etc/timezone" \
-    -and -not -path "./etc/localtime" \
-    -and -not -path "./opt/config/*" \
-    -and -not -path "./opt/PROGRAM/control/*" \
-    -and -not -path "./opt/PROGRAM/kernel/*" \
-    -and -not -path "./opt/PROGRAM/library/*" \
-    -and -not -path "./opt/PROGRAM/software/*" \
+    -and -not -path "./usr/data/config/*" \
+    -and -not -path "./usr/prog/PROGRAM/control/*" \
+    -and -not -path "./usr/prog/PROGRAM/kernel/*" \
+    -and -not -path "./usr/prog/PROGRAM/library/*" \
+    -and -not -path "./usr/prog/PROGRAM/software/*" \
     -and -not -path "./opt/auto_run.sh" \
-    -and -not -path "./root/version" \
-    -and -not -path "./root/.viminfo" \
-    -and -not -path "./opt/klipper/klippy/extras/spi_temperature.py" \
-    -and -not -path "./opt/klipper/klippy/extras/gcode_shell_command.py" \
-    -and -not -path "./opt/klipper/klippy/extras/buttons.py" \
-    -and -not -path "./opt/klipper/klippy/extras/save_variables.py" \
-    -and -not -path "./opt/klipper/klippy/mcu.py" \
-    -and -not -path "./opt/klipper/klippy/toolhead.py" \
-    -and -not -path "./opt/klipper/klippy/webhooks.py" \
-    -and -not -path "./opt/key.priv" \
-    -and -not -path "./opt/private.pem" \
-    -and -not -path "./opt/key.pub" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/spi_temperature.py" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/gcode_shell_command.py" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/buttons.py" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/save_variables.py" \
+    -and -not -path "./usr/prog/klipper/klippy/mcu.py" \
+    -and -not -path "./usr/prog/klipper/klippy/toolhead.py" \
+    -and -not -path "./usr/prog/klipper/klippy/webhooks.py" \
     -and -not -path "./etc/shadow-" \
     -and -not -path "./etc/shadow" \
-    -and -not -path "./etc/rtl_hostapd_2G.conf" \
-    -and -not -path "./etc/hosts" \
-    -and -not -path "./etc/random-seed" \
-    -and -not -path "./etc/MAC" \
-    -and -not -path "./etc/wpa_supplicant.conf" \
-    -and -not -path "./opt/tslib-1.12/etc/pointercal" \
-    -and -not -path "./Settings/Trolltech.conf" \
+    -and -not -path "./usr/prog/MAC" \
+    -and -not -path "./usr/prog/app_startup.sh" \
+    -and -not -path "./usr/prog/wifi/wpa_supplicant.conf" \
+    -and -not -path "/usr/prog/tslib-1.12/etc/pointercal" \
     -exec md5sum {} \; >md5sum.list
+
+exit
+echo "echo 'Проверка прав на файлы...'">>list.link
+find .  \
+    -type f \
+    -and -not -name "*.pyc" \
+    -and -not -path "./dev*" \
+    -and -not -path "./run*" \
+    -and -not -path "*__pycache__*" \
+    -and -not -name "md5sum.list" \
+    -and -not -name "md5.sh" \
+    -and -not -name "link.sh" \
+    -and -not -name "list.link" \
+    -and -not -path "./usr/data/config/*" \
+    -and -not -path "./usr/prog/PROGRAM/control/*" \
+    -and -not -path "./usr/prog/PROGRAM/kernel/*" \
+    -and -not -path "./usr/prog/PROGRAM/library/*" \
+    -and -not -path "./usr/prog/PROGRAM/software/*" \
+    -and -not -path "./opt/auto_run.sh" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/spi_temperature.py" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/gcode_shell_command.py" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/buttons.py" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/save_variables.py" \
+    -and -not -path "./usr/prog/klipper/klippy/mcu.py" \
+    -and -not -path "./usr/prog/klipper/klippy/toolhead.py" \
+    -and -not -path "./usr/prog/klipper/klippy/webhooks.py" \
+    -and -not -path "./etc/shadow-" \
+    -and -not -path "./etc/shadow" \
+    -and -not -path "./usr/prog/MAC" \
+    -and -not -path "./usr/prog/app_startup.sh" \
+    -and -not -path "./usr/prog/wifi/wpa_supplicant.conf" \
+    -and -not -path "/usr/prog/tslib-1.12/etc/pointercal" \
+    -exec ./link.sh {} "file" \; >>list.link
+
+echo "echo 'Проверка прав на каталоги...'">>list.link
+find .  \
+    -type d \
+    -and -not -name "*.pyc" \
+    -and -not -path "./dev*" \
+    -and -not -path "./run*" \
+    -and -not -path "*__pycache__*" \
+    -and -not -name "md5sum.list" \
+    -and -not -name "md5.sh" \
+    -and -not -name "link.sh" \
+    -and -not -name "list.link" \
+    -and -not -path "./usr/data/config/*" \
+    -and -not -path "./usr/prog/PROGRAM/control/*" \
+    -and -not -path "./usr/prog/PROGRAM/kernel/*" \
+    -and -not -path "./usr/prog/PROGRAM/library/*" \
+    -and -not -path "./usr/prog/PROGRAM/software/*" \
+    -and -not -path "./opt/auto_run.sh" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/spi_temperature.py" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/gcode_shell_command.py" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/buttons.py" \
+    -and -not -path "./usr/prog/klipper/klippy/extras/save_variables.py" \
+    -and -not -path "./usr/prog/klipper/klippy/mcu.py" \
+    -and -not -path "./usr/prog/klipper/klippy/toolhead.py" \
+    -and -not -path "./usr/prog/klipper/klippy/webhooks.py" \
+    -and -not -path "./etc/shadow-" \
+    -and -not -path "./etc/shadow" \
+    -and -not -path "./usr/prog/MAC" \
+    -and -not -path "./usr/prog/app_startup.sh" \
+    -and -not -path "./usr/prog/wifi/wpa_supplicant.conf" \
+    -and -not -path "/usr/prog/tslib-1.12/etc/pointercal" \
+    -exec ./link.sh {} "dir" \; >>list.link
