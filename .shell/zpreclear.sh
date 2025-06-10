d498bbe08934104d487bebcf73e3af6a58ba3374
Sergei Rozhkov <fgup@zserg.ru>
2025-06-10 09:36:28 +0500

    CANCEL_PRINT

--- .shell/zpreclear.sh
+++ .shell/zpreclear.sh
@@ -20,14 +20,14 @@ M190=$(head -1300 "${DATA_GCODES}/$1" | grep "^M190" | head -1)
 
 if [ "$M190" == "" ] || [ "$M109" == "" ]; then
     echo "RESPOND TYPE=command MSG=action:prompt_end" >/tmp/printer
+    echo "CANCEL_PRINT" >/tmp/printer
     if [ ${ZLANG} != 'ru' ]; then
         echo "RESPOND TYPE=error MSG=\"File $1 does not contain bed heating commands (M140/M190) or nozzle heating commands (M104/M109). They must be in the first 1300 lines. G-code thumbnails 140x110/PNG\"" >/tmp/printer
-        echo "RESPOND TYPE=error MSG=\"Печать отменена\"" >/tmp/printer
+        echo "RESPOND TYPE=error MSG=\"Printing cancelled\"" >/tmp/printer
     else
         echo "RESPOND TYPE=error MSG=\"В файле $1 не найдены команды нагрева стола(M140/M190) или сопла(M104/M109). Они должны быть в первой 1300 строк. Эскизы G-кода 140x110/PNG\"" >/tmp/printer
-        echo "RESPOND TYPE=error MSG=\"Printing cancelled\"" >/tmp/printer
+        echo "RESPOND TYPE=error MSG=\"Печать отменена\"" >/tmp/printer
     fi
-    echo "CANCEL_PRINT" >/tmp/printer
     exit 1
 fi
 
