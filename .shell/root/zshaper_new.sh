96be7da5571160909b693ca41bce41465404c24e
Sergei Rozhkov <fgup@zserg.ru>
2025-08-15 07:48:40 +0500

    ZSHAPER

--- .shell/root/zshaper_new.sh
+++ .shell/root/zshaper_new.sh
@@ -30,16 +30,20 @@ DT=$(date '+%Y%m%d_%H%M')
 cd /opt/config/mod_data/
 mkdir -p shapers
 
-[ ${ZLANG} != 'ru' ] && echo "Preparing X-axis image. Please wait..." || echo "Подготовка изображения оси X. Ждите..."
-sed 's/psd_x/psd_Y/' /tmp/resonances_x_x.csv | sed 's/psd_y/psd_x/' | sed 's/psd_Y/psd_y/' | awk -F ',' '{print $1","$3","$2","$4","$5}' >X
-python3 /opt/config/mod/.shell/root/zshaper/calibrate_shaper.py X --scv=$SCV -o resonances_x.png -w 8 -l 4.8 --send X --${ZLANG}
-mv X shapers/X_$DT.csv
-cp resonances_x.png shapers/calibration_data_X_$DT.png
+if [ "$1" == "X" ]; then
+    b_low="x";
+else if [ "$1" == "Y" ]; then
+    b_low="y";
+else
+    echo "Incorrect $1 != X or $1 != Y"
+    exit
+fi
+fi
 
-[ ${ZLANG} != 'ru' ] && echo "Preparing Y-axis image. Please wait..." || echo "Подготовка изображения оси Y. Ждите..."
-sed 's/psd_x/psd_Y/' /tmp/resonances_y_y.csv | sed 's/psd_y/psd_x/' | sed 's/psd_Y/psd_y/' | awk -F ',' '{print $1","$3","$2","$4","$5}' >Y
-python3 /opt/config/mod/.shell/root/zshaper/calibrate_shaper.py Y --scv=$SCV -o resonances_y.png -w 8 -l 4.8 --send Y --${ZLANG}
-mv Y shapers/Y_$DT.csv
-cp resonances_y.png shapers/calibration_data_Y_$DT.png
+[ ${ZLANG} != 'ru' ] && echo "Preparing $1-axis image. Please wait..." || echo "Подготовка изображения оси $1. Ждите..."
+sed 's/psd_x/psd_Y/' /tmp/resonances_${b_low}_${b_low}.csv | sed 's/psd_y/psd_x/' | sed 's/psd_Y/psd_y/' | awk -F ',' '{print $1","$3","$2","$4","$5}' >$1
+python3 /opt/config/mod/.shell/root/zshaper/calibrate_shaper.py $1 --scv=$SCV -o resonances_$1.png -w 8 -l 4.8 --send $1 --${ZLANG}
+mv $1 shapers/$1_$DT.csv
+cp resonances_$1.png shapers/calibration_data_$1_$DT.png
 
-[ ${ZLANG} != 'ru' ] && echo "Images are available in Configuration -> mod_data resonances_x.png and resonances_y.png." || echo "Изображения лежат во вкладке Конфигурация -> mod_data resonances_x.png и resonances_y.png."
+[ ${ZLANG} != 'ru' ] && echo "Images are available in Configuration -> mod_data resonances_$1.png." || echo "Изображения лежат во вкладке Конфигурация -> mod_data resonances_$1.png."
