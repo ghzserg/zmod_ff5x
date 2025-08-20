557905261ddacc8a8578d2d51a5d20d52135e2ed
Sergei Rozhkov <fgup@zserg.ru>
2025-08-20 12:57:27 +0500

    zshaper_new.sh

--- .shell/root/zshaper_new.sh
+++ .shell/root/zshaper_new.sh
@@ -2,22 +2,29 @@
 
 source /opt/config/mod/.shell/0.sh
 
+for arg in "$@"; do
+    if [ "$arg" = "--json" ]; then
+        json_present=true
+        break
+    fi
+done
+
 SCV="5.0"
 if grep -q "fix_scv = 1" /opt/config/mod_data/variables.cfg; then
     if grep -q '^square_corner_velocity' /opt/config/mod_data/user.cfg; then
         SCV=$(grep '^square_corner_velocity' /opt/config/mod_data/user.cfg| cut -d ":" -f 2 | awk '{print $1}')
-        [ ${ZLANG} != 'ru' ] && echo "Using SCV (square_corner_velocity) = $SCV from mod_data/user.cfg" || echo "Используется SCV (square_corner_velocity) = $SCV из mod_data/user.cfg"
+        if ! $json_present; then [ ${ZLANG} != 'ru' ] && echo "Using SCV (square_corner_velocity) = $SCV from mod_data/user.cfg" || echo "Используется SCV (square_corner_velocity) = $SCV из mod_data/user.cfg"; fi
     else if grep -q '^square_corner_velocity' /opt/config/printer.base.cfg; then
         SCV=$(grep '^square_corner_velocity' /opt/config/printer.base.cfg| cut -d ":" -f 2 | awk '{print $1}')
-        [ ${ZLANG} != 'ru' ] && echo "Using SCV (square_corner_velocity) = $SCV from printer.base.cfg" || echo "Используется SCV (square_corner_velocity) = $SCV из printer.base.cfg"
+        if ! $json_present; then [ ${ZLANG} != 'ru' ] && echo "Using SCV (square_corner_velocity) = $SCV from printer.base.cfg" || echo "Используется SCV (square_corner_velocity) = $SCV из printer.base.cfg"; fi
     else
-        [ ${ZLANG} != 'ru' ] && echo "Using default SCV (square_corner_velocity) = $SCV" || echo "Используется стандартный SCV (square_corner_velocity) = $SCV"
+        if ! $json_present; then [ ${ZLANG} != 'ru' ] && echo "Using default SCV (square_corner_velocity) = $SCV" || echo "Используется стандартный SCV (square_corner_velocity) = $SCV"; fi
     fi
     fi
 fi
 
 SCV_INT="${SCV%%.*}"
-if [ "$SCV_INT" -ge 11 ]; then
+if [ "$SCV_INT" -ge 11 ] && ! $json_present; then
     if [ "${ZLANG}" != 'ru' ]; then
         echo "!! SCV($SCV) too high detected // https://github.com/ghzserg/zmod/wiki/Global_en#fix_scv !!"
     else
@@ -44,6 +51,7 @@ fi
 sed 's/psd_x/psd_Y/' /tmp/resonances_${b_low}_${b_low}.csv | sed 's/psd_y/psd_x/' | sed 's/psd_Y/psd_y/' | awk -F ',' '{print $1","$3","$2","$4","$5}' >$1
 python3 /opt/config/mod/.shell/root/zshaper/calibrate_shaper.py $1 --scv=$SCV -o resonances_$1.png -w 8 -l 4.8 --send $1 --${ZLANG}
 mv $1 shapers/$1_$DT.csv
-cp resonances_$1.png shapers/calibration_data_$1_$DT.png
-
-[ ${ZLANG} != 'ru' ] && echo "Images are available in Configuration -> mod_data resonances_$1.png." || echo "Изображения лежат во вкладке Конфигурация -> mod_data resonances_$1.png."
+if ! $json_present; then
+    cp resonances_$1.png shapers/calibration_data_$1_$DT.png
+    [ ${ZLANG} != 'ru' ] && echo "Images are available in Configuration -> mod_data resonances_$1.png." || echo "Изображения лежат во вкладке Конфигурация -> mod_data resonances_$1.png."
+fi
--- .shell/zshaper_new.sh
+++ .shell/zshaper_new.sh
@@ -7,7 +7,19 @@ unset LD_PRELOAD
 if [ -f /ZMOD ]; then
     /opt/config/mod/.shell/root/zshaper_new.sh $@
 else
-    [ ${FF5X} -eq 0 ] && while umount ${UMOUNT_MOD} 2>/dev/null; do a=b; done
-    chroot ${MOD} /opt/config/mod/.shell/root/zshaper_new.sh $@
-    [ ${FF5X} -eq 0 ] && mount --bind ${REMOUNT_MOD} ${UMOUNT_MOD}
+    if [ ${FF5X} -eq 0 ]; then
+        while umount ${UMOUNT_MOD} 2>/dev/null; do a=b; done
+        chroot ${MOD} /opt/config/mod/.shell/root/zshaper_new.sh $@
+        mount --bind ${REMOUNT_MOD} ${UMOUNT_MOD}
+    else
+        export PATH=$PATH:/usr/prog/Python-3.8.2/bin
+        export LD_LIBRARY_PATH=/usr/prog/Python-3.8.2/lib:$LD_LIBRARY_PATH
+        export LD_LIBRARY_PATH=/usr/prog/openssl-1.0.2d/lib:$LD_LIBRARY_PATH
+        export LD_LIBRARY_PATH=/usr/prog/libffi-3.4.4/lib:$LD_LIBRARY_PATH
+        /opt/config/mod/.shell/root/zshaper_new.sh --json $@
+
+        unset LD_LIBRARY_PATH
+        export PATH='/bin:/sbin:/usr/bin:/usr/sbin'
+        chroot ${MOD} /opt/config/mod/.shell/root/zshaper_new.sh --json-in $@
+    fi
 fi
