c3ecc09628ec3b38e20b4e43fc7ee95fcdcdf9e9
Sergei Rozhkov <fgup@zserg.ru>
2025-08-20 13:16:24 +0500

    json

--- .shell/zshaper_new.sh
+++ .shell/zshaper_new.sh
@@ -16,10 +16,10 @@ else
         export LD_LIBRARY_PATH=/usr/prog/Python-3.8.2/lib:$LD_LIBRARY_PATH
         export LD_LIBRARY_PATH=/usr/prog/openssl-1.0.2d/lib:$LD_LIBRARY_PATH
         export LD_LIBRARY_PATH=/usr/prog/libffi-3.4.4/lib:$LD_LIBRARY_PATH
-        /opt/config/mod/.shell/root/zshaper_new.sh --json $@
+        /opt/config/mod/.shell/root/zshaper_new.sh $@ --json
 
         unset LD_LIBRARY_PATH
         export PATH='/bin:/sbin:/usr/bin:/usr/sbin'
-        chroot ${MOD} /opt/config/mod/.shell/root/zshaper_new.sh --json-in $@
+        chroot ${MOD} /opt/config/mod/.shell/root/zshaper_new.sh $@ --json-in
     fi
 fi
