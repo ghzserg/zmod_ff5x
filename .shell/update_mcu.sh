c20065913bb94e158e7f90b6583be076b67d176c
Sergei Rozhkov <fgup@zserg.ru>
2025-08-25 15:00:14 +0500

    update_mcu

--- .shell/update_mcu.sh
+++ .shell/update_mcu.sh
@@ -4,19 +4,26 @@
 
 set -x
 
-WORK_DIR=$(pwd)
-
-export PATH=/bin:/sbin:/usr/bin:/usr/sbin
-
-FIRMWARE_Board_M3=/opt/config/mod/.shell/root/mcu/Mainboard.bin
-FIRMWARE_Head_M3=/opt/config/mod/.shell/root/mcu/Eboard.hex
-
 CHECH_ARCH=`uname -m`
-if [ "${CHECH_ARCH}" != "armv7l" ];then
+if [ "${CHECH_ARCH}" == "armv7l" ];then
+    WORK_DIR=$(pwd)
+    CONFIG_DIR="/opt/config/"
+    EBOARD_TTY="/dev/ttyS1"
+else if [ "${CHECH_ARCH}" == "mips" ];then
+    export PATH=/bin:/sbin:/usr/bin:/usr/sbin
+    CONFIG_DIR="/usr/data/config"
+    WORK_DIR=`dirname $0`
+    EBOARD_TTY="/dev/ttyS5"
+else
     echo "Machine architecture error."
     echo ${CHECH_ARCH}
     exit 1
 fi
+fi
+
+FIRMWARE_Head_M3="${CONFIG_DIR}/mod/.shell/root/mcu/Eboard.hex"
+FIRMWARE_Board_M3="${CONFIG_DIR}/mod/.shell/root/mcu/Mainboard.bin"
+
 
 cat $WORK_DIR/mcu.img > /dev/fb0
 
@@ -24,52 +31,72 @@ update_mcu_mainboard()
 {
     if [ -f $WORK_DIR/NationsCommand ];then
 	chmod a+x $WORK_DIR/NationsCommand
-	if [ -f $FIRMWARE_Board_M3 ];then
+	if [ -f $WORK_DIR/$FIRMWARE_Board_M3 ];then
 		echo "burn M3 firmware..."
-		$WORK_DIR/NationsCommand -c -d --fn $FIRMWARE_Board_M3 --v -r
+		$WORK_DIR/NationsCommand -c -d --fn $WORK_DIR/$FIRMWARE_Board_M3 --v -r
 	fi
     fi
-
 }
 
 update_mcu_eboard()
 {
     if [ -f $WORK_DIR/IAPCommand ];then
 	chmod a+x $WORK_DIR/IAPCommand
-	if [ -f $FIRMWARE_Head_M3 ];then
+	if [ -f $WORK_DIR/$FIRMWARE_Head_M3 ];then
 		echo "burn M3 firmware..."
-		$WORK_DIR/IAPCommand $FIRMWARE_Head_M3 /dev/ttyS1
+		$WORK_DIR/IAPCommand $WORK_DIR/$FIRMWARE_Head_M3 $EBOARD_TTY
 		sync
 	fi
+    fi
+}
+
+update_mcu_ifs()
+{
+    if [ -f $WORK_DIR/IFSCommand ]; then
+        echo "update ifs"
+        cp -f $WORK_DIR/IFSCommand  /usr/prog/PROGRAM/control/
+        cp -f $WORK_DIR/ifs.hex  /usr/prog/PROGRAM/control/
+        chmod a+x $WORK_DIR/ifsF37
+        $WORK_DIR/ifsF37 /dev/ttyS4
+        $WORK_DIR/IFSCommand $WORK_DIR/ifs.hex /dev/ttyS4
     fi
 }
 
 killall python3.7 firmwareExe
 kill $(ps|grep klippy.py| grep -v grep| awk '{print $1}')
 
-mkdir -p /opt/config/mod_data/log/
+mkdir -p ${CONFIG_DIR}/mod_data/log/
 
 if [ "$1" == "mainboard" ]; then
-    mv /opt/config/mod_data/log/update_mcu_mainboard.4.log /opt/config/mod_data/log/update_mcu_mainboard.5.log
-    mv /opt/config/mod_data/log/update_mcu_mainboard.3.log /opt/config/mod_data/log/update_mcu_mainboard.4.log
-    mv /opt/config/mod_data/log/update_mcu_mainboard.2.log /opt/config/mod_data/log/update_mcu_mainboard.3.log
-    mv /opt/config/mod_data/log/update_mcu_mainboard.1.log /opt/config/mod_data/log/update_mcu_mainboard.2.log
-    mv /opt/config/mod_data/log/update_mcu_mainboard.log /opt/config/mod_data/log/update_mcu_mainboard.1.log
+    mv ${CONFIG_DIR}/mod_data/log/update_mcu_mainboard.4.log ${CONFIG_DIR}/mod_data/log/update_mcu_mainboard.5.log
+    mv ${CONFIG_DIR}/mod_data/log/update_mcu_mainboard.3.log ${CONFIG_DIR}/mod_data/log/update_mcu_mainboard.4.log
+    mv ${CONFIG_DIR}/mod_data/log/update_mcu_mainboard.2.log ${CONFIG_DIR}/mod_data/log/update_mcu_mainboard.3.log
+    mv ${CONFIG_DIR}/mod_data/log/update_mcu_mainboard.1.log ${CONFIG_DIR}/mod_data/log/update_mcu_mainboard.2.log
+    mv ${CONFIG_DIR}/mod_data/log/update_mcu_mainboard.log ${CONFIG_DIR}/mod_data/log/update_mcu_mainboard.1.log
 
-    update_mcu_mainboard &>/opt/config/mod_data/log/update_mcu_mainboard.log
-    /opt/config/mod/.shell/root/audio/audio_midi.sh For_Elise.mid
+    update_mcu_mainboard &>${CONFIG_DIR}/mod_data/log/update_mcu_mainboard.log
+    ${CONFIG_DIR}/mod/.shell/root/audio/audio_midi.sh For_Elise.mid
     sync
     sleep 5
     poweroff
 else
-    mv /opt/config/mod_data/log/update_mcu_eboard.4.log /opt/config/mod_data/log/update_mcu_eboard.5.log
-    mv /opt/config/mod_data/log/update_mcu_eboard.3.log /opt/config/mod_data/log/update_mcu_eboard.4.log
-    mv /opt/config/mod_data/log/update_mcu_eboard.2.log /opt/config/mod_data/log/update_mcu_eboard.3.log
-    mv /opt/config/mod_data/log/update_mcu_eboard.1.log /opt/config/mod_data/log/update_mcu_eboard.2.log
-    mv /opt/config/mod_data/log/update_mcu_eboard.log /opt/config/mod_data/log/update_mcu_eboard.1.log
+    mv ${CONFIG_DIR}/mod_data/log/update_mcu_eboard.4.log ${CONFIG_DIR}/mod_data/log/update_mcu_eboard.5.log
+    mv ${CONFIG_DIR}/mod_data/log/update_mcu_eboard.3.log ${CONFIG_DIR}/mod_data/log/update_mcu_eboard.4.log
+    mv ${CONFIG_DIR}/mod_data/log/update_mcu_eboard.2.log ${CONFIG_DIR}/mod_data/log/update_mcu_eboard.3.log
+    mv ${CONFIG_DIR}/mod_data/log/update_mcu_eboard.1.log ${CONFIG_DIR}/mod_data/log/update_mcu_eboard.2.log
+    mv ${CONFIG_DIR}/mod_data/log/update_mcu_eboard.log ${CONFIG_DIR}/mod_data/log/update_mcu_eboard.1.log
 
-    update_mcu_eboard &>/opt/config/mod_data/log/update_mcu_eboard.log
-    /opt/config/mod/.shell/root/audio/audio_midi.sh For_Elise.mid
+    update_mcu_eboard &>${CONFIG_DIR}/mod_data/log/update_mcu_eboard.log
+
+    mv ${CONFIG_DIR}/mod_data/log/update_mcu_ifs.4.log ${CONFIG_DIR}/mod_data/log/update_mcu_ifs.5.log
+    mv ${CONFIG_DIR}/mod_data/log/update_mcu_ifs.3.log ${CONFIG_DIR}/mod_data/log/update_mcu_ifs.4.log
+    mv ${CONFIG_DIR}/mod_data/log/update_mcu_ifs.2.log ${CONFIG_DIR}/mod_data/log/update_mcu_ifs.3.log
+    mv ${CONFIG_DIR}/mod_data/log/update_mcu_ifs.1.log ${CONFIG_DIR}/mod_data/log/update_mcu_ifs.2.log
+    mv ${CONFIG_DIR}/mod_data/log/update_mcu_ifs.log ${CONFIG_DIR}/mod_data/log/update_mcu_ifs.1.log
+
+    update_mcu_ifs &>${CONFIG_DIR}/mod_data/log/update_mcu_ifs.log
+
+    ${CONFIG_DIR}/mod/.shell/root/audio/audio_midi.sh For_Elise.mid
     sync
 fi
 
