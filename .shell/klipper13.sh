8ea65f4308242573b51890a0684c70ae512ee3ac
Sergei Rozhkov <fgup@zserg.ru>
2025-08-25 16:26:01 +0500

    Klipper 13

--- .shell/klipper13.sh
+++ .shell/klipper13.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-if [ -f /usr/data/config/mod/.shell/fix_config.sh ] && /usr/data/config/mod/.shell/fix_config.sh start
+PROG_DIR="/opt"
+if [ -f /usr/data/config/mod/.shell/fix_config.sh ]; then
+    /usr/data/config/mod/.shell/fix_config.sh start
+    PROG_DIR="/usr/prog"
+fi
 
-find /opt/PROGRAM/control/ -name NationsCommand| while read a; do $a -r ; done;
+find ${PROG_DIR}/PROGRAM/control/ -name NationsCommand| while read a; do $a -r ; done;
 exit 0
