854d399be02a07ea0f9af423b365f53058feb89e
Sergei Rozhkov <fgup@zserg.ru>
2025-08-28 10:19:39 +0500

    IN

--- .shell/zmod_color.py
+++ .shell/zmod_color.py
@@ -1132,7 +1132,7 @@ class zmod_color:
                 payload = {
                     "cmd": "ipdMs_cmd",
                     "args": {
-                        "action": action
+                        "action": napr
                     }
                 }
 
