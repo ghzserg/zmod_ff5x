2f50b05d4d4db32cc269414ee486b8b32ba84079
Sergei Rozhkov <fgup@zserg.ru>
2025-09-05 22:12:35 +0500

    Klipper 13

--- .shell/root/start.sh
+++ .shell/root/start.sh
@@ -102,10 +102,11 @@ grep -q "PRETTY_NAME=\"${V1} -> ${V2}\"" /etc/os-release || sed -i "s|PRETTY_NAM
 
 mkdir -p ${DATA_GCODES}/tmp
 
-[ ${FF5X} -eq 0 ] && mount --bind ${REMOUNT_MOD} ${UMOUNT_MOD}
-
-if grep -q "klipper13 = 1" /opt/config/mod_data/variables.cfg; then
-    /opt/config/mod/.shell/root/S60klipper start
+if [ ${FF5X} -eq 0 ]; then
+    mount --bind ${REMOUNT_MOD} ${UMOUNT_MOD}
+    if grep -q "klipper13 = 1" /opt/config/mod_data/variables.cfg; then
+        /opt/config/mod/.shell/root/S60klipper start
+    fi
 fi
 
 /opt/config/mod/.shell/root/S65moonraker start
