411da320eefa095ad5b2f13f216681e687dcb7e1
Sergei Rozhkov <fgup@zserg.ru>
2025-10-10 19:44:51 +0500

    ZEXCLUDE

--- .shell/zmod.py
+++ .shell/zmod.py
@@ -1,12 +1,56 @@
+import os
+
 class zmod:
     def __init__(self, config):
         self.printer = config.get_printer()
         self.language = config.get('language', 'en')
-        gcode = self.printer.lookup_object('gcode')
-        gcode.register_command('SAVE_SHAPER', self.cmd_SAVE_SHAPER)
+        self.gcode = self.printer.lookup_object('gcode')
+        gcode_macro = self.printer.load_object(config, 'gcode_macro')
+        self.on_error_gcode = gcode_macro.load_template(
+            config, 'on_error_gcode', '')
+
+        self.virtual_sdcard = self.printer.lookup_object('virtual_sdcard')
+        self.sdcard_dirname = self.virtual_sdcard.sdcard_dirname
+
+        self.gcode.register_command('SAVE_SHAPER', self.cmd_SAVE_SHAPER)
+        self.gcode.register_command('ZEXCLUDE', self.cmd_ZEXCLUDE)
 
     def get_lang(self):
         return self.language
+
+    def cmd_ZEXCLUDE(self, gcmd):
+        filename = gcmd.get("FILENAME")
+        if filename[0] == '/':
+            filename = filename[1:]
+        if not filename:
+            raise gcmd.error("FILENAME is required")
+
+        full_path = os.path.join(self.sdcard_dirname, filename)
+
+        try:
+            with open(full_path, 'r') as f:
+                for line in f:
+                    line = line.strip()
+                    if not line or line.startswith('#'):
+                        continue
+
+                    if line.startswith('EXCLUDE_OBJECT_DEFINE'):
+                        try:
+                            self.gcode.run_script(line)
+                        except self.gcode.error as e:
+                            error_message = str(e)
+                            try:
+                                self.gcode.run_script(self.on_error_gcode.render())
+                            except:
+                                logging.exception("zexclude_error")
+                            break
+                        except:
+                            logging.exception("zexclude_error")
+                            break
+        except FileNotFoundError:
+            gcmd.respond_error(f"File not found: {full_path}")
+        except Exception:
+            logging.exception("zexclude_file_error")
 
     def cmd_SAVE_SHAPER(self, gcmd):
         shaper_name = gcmd.get('NAME', '').lower()
