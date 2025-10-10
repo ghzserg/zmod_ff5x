4a072b1a4ac5be240a872cafe334d8b7c0d22f47
function3d <109918879+function3d@users.noreply.github.com>
2025-10-10 16:26:29 +0200

    filament types (unused)

--- ifs_colors.py
+++ ifs_colors.py
@@ -5,6 +5,7 @@ gcode = ''
 output = []
 tools = set(['0'])
 colors = []
+types = []
 t = ''
 print(sys.argv[1])
 with open(sys.argv[1], 'r') as gcode:
@@ -17,11 +18,14 @@ with open(sys.argv[1], 'r') as gcode:
         elif re.match("; filament_colour = (.*)", line):
             output.append(line)
             colors = re.match("; filament_colour = (.*)", line).group(1).split(";")
+        elif re.match("; filament_type = (.*)", line):
+            output.append(line)
+            types = re.match("; filament_type = (.*)", line).group(1).split(";")
         else:
             output.append(line)
             
 
 with open(sys.argv[1], 'w') as f:
-    f.write("_IFS_COLORS START=1 COLORS=" + ",".join("{0}".format(c[1:]) for c in colors) + " TOOLS=" + ",".join(sorted(tools)) + "\n") 
+    f.write("_IFS_COLORS START=1 TYPES=" + ",".join(types) + " COLORS=" + ",".join("{0}".format(c[1:]) for c in colors) + " TOOLS=" + ",".join(sorted(tools)) + "\n") 
     for line in output:
         f.write(line)
