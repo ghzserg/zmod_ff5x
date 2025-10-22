fe2df42448d101cb26b9bcd21845d6417f80f08e
Sergei Rozhkov <fgup@zserg.ru>
2025-10-22 11:51:33 +0500

    Plugins.sh

--- .shell/plugins.sh
+++ .shell/plugins.sh
@@ -56,15 +56,17 @@ if grep -q "[update_manager $1]" ${MOD_CONF}/moonraker.conf || grep -q "[update_
     fi
 
     if ! [ -f "${MOD_CONF}/mod_data/plugins/$1/$1.cfg" ] && ! [ -f "${MOD_CONF}/mod_data/plugins/$1/${ZLANG}/$1.cfg" ]; then
-        if [ ${ZLANG} != 'ru' ]; then
+        if [ "${ZLANG}" != 'ru' ]; then
             echo "Plugin $1 not found in mod_data/plugins/$1/$1.cfg and mod_data/plugins/$1/${ZLANG}/$1.cfg"
         else
             echo "Основной файл плагина $1 не найден в mod_data/plugins/$1/$1.cfg и mod_data/plugins/$1/${ZLANG}/$1.cfg"
         fi
         exit 1
     fi
+
+    cd "${MOD_CONF}/mod_data/plugins/$1"
     if [ "$2" == "Enable" ]; then
-        if [ ${ZLANG} != 'ru' ]; then
+        if [ "${ZLANG}" != 'ru' ]; then
             echo "Enable plugin $1"
         else
             echo "Включаю плагин $1"
@@ -76,17 +78,20 @@ if grep -q "[update_manager $1]" ${MOD_CONF}/moonraker.conf || grep -q "[update_
         else
             echo "[include plugins/$1/$1.cfg]" >>${MOD_CONF}/mod_data/plugins.cfg
         fi
+
+        [ -x "${MOD_CONF}/mod_data/plugins/$1/install.sh" ] && "${MOD_CONF}/mod_data/plugins/$1/install.sh"
     else
-        if [ ${ZLANG} != 'ru' ]; then
+        if [ "${ZLANG}" != 'ru' ]; then
             echo "Disable plugin $1"
         else
             echo "Выключаю плагин $1"
         fi
         sed -i "/plugins\/$1\//d" ${MOD_CONF}/mod_data/plugins.cfg
+        [ -x "${MOD_CONF}/mod_data/plugins/$1/uninstall.sh" ] && "${MOD_CONF}/mod_data/plugins/$1/uninstall.sh"
     fi
     echo "FIRMWARE_RESTART" >/tmp/printer
 else
-    if [ ${ZLANG} != 'ru' ]; then
+    if [ "${ZLANG}" != 'ru' ]; then
         echo "Plugin $1 not found in moonraker.conf, mod_data/user.moonraker.conf"
     else
         echo "Плагин $1 не найден в файлах moonraker.conf, mod_data/user.moonraker.conf"
