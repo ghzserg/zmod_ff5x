348d05f65a5c38308a903784a4dd7fff10475d96
Sergei Rozhkov <fgup@zserg.ru>
2025-12-15 13:37:04 +0500

    Update

--- .shell/check_start_print.sh
+++ .shell/check_start_print.sh
@@ -12,8 +12,8 @@ fi
 source /opt/config/mod/.shell/0.sh
 
 if ! awk '
-    /END_PRINT/   { end_found = 1 }
-    /START_PRINT/ { start_found = 1 }
+    /^END_PRINT/   { end_found = 1 }
+    /^START_PRINT/ { start_found = 1 }
     END { exit !(end_found && start_found) }
 ' "${FILE_NAME}"; then
     [ ${ZLANG} == 'ru' ] && echo 'RESPOND PREFIX="!!" MSG="Макрос START_PRINT или END_PRINT не найден в файле. При работе без родного экрана они должны быть. https://github.com/ghzserg/zmod/wiki/FAQ"' >/tmp/printer || echo 'The START_PRINT or END_PRINT macros were not found in the file. They should be present when working without a native screen. https://github.com/ghzserg/zmod/wiki/FAQ_en' >/tmp/printer
