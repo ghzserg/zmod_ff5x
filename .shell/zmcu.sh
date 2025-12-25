c31aa053eb460e189884d1c1b5c00e466908990e
Sergei Rozhkov <fgup@zserg.ru>
2025-12-25 17:00:19 +0500

    ZMCU

--- .shell/zmcu.sh
+++ .shell/zmcu.sh
@@ -3,6 +3,12 @@
 source /opt/config/mod/.shell/0.sh
 set -x
 
+if grep -q bambufy /opt/config/mod_data/plugins.cfg; then
+    echo "RESPOND TYPE=command MSG=action:prompt_end" >/tmp/printer
+    [ ${ZLANG} != 'ru' ] && echo "RESPOND TYPE=error MSG=\"Отключите плагин Bambufy. DISABLE_PLUGIN name=bambufy\"">/tmp/printer || echo "RESPOND TYPE=error MSG=\"Disable plugin Bambufy. DISABLE_PLUGIN name=bambufy\"">/tmp/printer
+    exit 0
+fi
+
 CONTROL_DIR=${PROGRAM_DIR}control/
 cd ${CONTROL_DIR}
 if [ ${FF5X} -eq 1 ]; then
@@ -19,12 +25,14 @@ echo "">${CONTRIL_FLAG}
 if [ -d "${CONTROL_DIR}${CONTROL_VERSION}" ]; then
     cd "${CONTROL_DIR}${CONTROL_VERSION}"
     if [ "$1" -eq 1 ]; then
+        echo "SAVE_VARIABLE VARIABLE=klipper13 VALUE=1" >/tmp/printer
         if ! [ -f /ZMOD ]; then
             start-stop-daemon -S -b -x /opt/config/mod/.shell/update_mcu.sh -- mainboard
         else
             /opt/config/mod/.shell/zremote.sh /opt/config/mod/.shell/zmcu_13.sh "${CONTROL_DIR}${CONTROL_VERSION}"
         fi
     else
+        echo "SAVE_VARIABLE VARIABLE=klipper13 VALUE=0" >/tmp/printer
         /opt/config/mod/.shell/root/audio/audio_midi.sh For_Elise.mid
         sync
         sleep 5
