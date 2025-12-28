1b7a088b79b4ac510df6607bb9f7b6b4ce2e4f4a
Sergei Rozhkov <fgup@zserg.ru>
2025-12-28 19:33:55 +0500

    Wifi

--- .shell/wifi_reconnect.sh
+++ .shell/wifi_reconnect.sh
@@ -11,14 +11,14 @@ case "$2" in
     CONNECTED)
         echo "connection established"
         echo "$(date): Wi-Fi connected, starting DHCP on $1" >>${MOD_CONF}/mod_data/log/wifi.log
-        killall udhcpc 2>&1 >>${MOD_CONF}/mod_data/log/wifi.log
-        ip addr flush dev "$1" 2>&1  >>${MOD_CONF}/mod_data/log/wifi.log
-        udhcpc -i "$1" -b -R -T 3 -t 5 2>&1 >>${MOD_CONF}/mod_data/log/wifi.log
-        ifconfig "$1" 2>&1 >>${MOD_CONF}/mod_data/log/wifi.log
+        killall udhcpc >>${MOD_CONF}/mod_data/log/wifi.log 2>&1
+        ip addr flush dev "$1" >>${MOD_CONF}/mod_data/log/wifi.log 2>&1
+        udhcpc -i "$1" -b -R -T 3 -t 5 >>${MOD_CONF}/mod_data/log/wifi.log 2>&1
+        ifconfig "$1" >>${MOD_CONF}/mod_data/log/wifi.log 2>&1
         ;;
     DISCONNECTED)
         echo "connection lost"
         echo "$(date): Wi-Fi disconnected on $1" >>${MOD_CONF}/mod_data/log/wifi.log
-        killall udhcpc 2>&1 >>${MOD_CONF}/mod_data/log/wifi.log
+        killall udhcpc >>${MOD_CONF}/mod_data/log/wifi.log 2>&1
         ;;
 esac
