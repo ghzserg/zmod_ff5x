80da6fe1226f41b0aae9a8c5be92b5166b3b9af1
Sergei Rozhkov <fgup@zserg.ru>
2026-01-15 20:11:32 +0500

    Guppy

--- .shell/zmod.sh
+++ .shell/zmod.sh
@@ -1,23 +1,33 @@
 #!/bin/sh
 # (C) 2024-2026 ghzserg https://github.com/ghzserg/zmod
 
+source /opt/config/mod/.shell/0.sh
+
 up()
 {
     if ! [ -f /ZMOD ]; then
-        /opt/config/mod/.shell/S99moon up
+        #/opt/config/mod/.shell/S99moon up
+        chroot ${MOD} /etc/init.d/S65moonraker start
+        chroot ${MOD} /etc/init.d/S70httpd start
+        chroot ${MOD} /etc/init.d/S80guppyscreen start
     else
         /etc/init.d/S65moonraker start
         /etc/init.d/S70httpd start
+        /etc/init.d/S80guppyscreen start
     fi
 }
 
 stop()
 {
     if ! [ -f /ZMOD ]; then
-        /opt/config/mod/.shell/S99moon stop
+        #/opt/config/mod/.shell/S99moon stop
+        chroot ${MOD} /etc/init.d/S65moonraker stop
+        chroot ${MOD} /etc/init.d/S70httpd stop
+        chroot ${MOD} /etc/init.d/S80guppyscreen stop
     else
         /etc/init.d/S65moonraker stop
         /etc/init.d/S70httpd stop
+        /etc/init.d/S80guppyscreen stop
     fi
 }
 
