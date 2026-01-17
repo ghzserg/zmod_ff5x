66c5f4a18d02bb26c20d999a6ebbaa14f8cb15b1
Sergei Rozhkov <fgup@zserg.ru>
2026-01-17 14:41:32 +0500

    STOP_ZMOD

--- .shell/zmod.sh
+++ .shell/zmod.sh
@@ -7,13 +7,13 @@ up()
 {
     if ! [ -f /ZMOD ]; then
         #/opt/config/mod/.shell/S99moon up
-        chroot ${MOD} /etc/init.d/S65moonraker start
-        chroot ${MOD} /etc/init.d/S70httpd start
-        chroot ${MOD} /etc/init.d/S80guppyscreen start
+        [ "$2" -eq 1 ] && chroot ${MOD} /etc/init.d/S80guppyscreen start
+        [ "$3" -eq 1 ] && chroot ${MOD} /etc/init.d/S65moonraker start
+        [ "$4" -eq 1 ] && chroot ${MOD} /etc/init.d/S70httpd start
     else
-        /etc/init.d/S65moonraker start
-        /etc/init.d/S70httpd start
-        /etc/init.d/S80guppyscreen start
+        [ "$2" -eq 1 ] && /etc/init.d/S80guppyscreen start
+        [ "$3" -eq 1 ] && /etc/init.d/S65moonraker start
+        [ "$4" -eq 1 ] && /etc/init.d/S70httpd start
     fi
 }
 
@@ -21,13 +21,13 @@ stop()
 {
     if ! [ -f /ZMOD ]; then
         #/opt/config/mod/.shell/S99moon stop
-        chroot ${MOD} /etc/init.d/S65moonraker stop
-        chroot ${MOD} /etc/init.d/S70httpd stop
-        chroot ${MOD} /etc/init.d/S80guppyscreen stop
+        [ "$2" -eq 1 ] && chroot ${MOD} /etc/init.d/S80guppyscreen stop
+        [ "$3" -eq 1 ] && chroot ${MOD} /etc/init.d/S65moonraker stop
+        [ "$4" -eq 1 ] && chroot ${MOD} /etc/init.d/S70httpd stop
     else
-        /etc/init.d/S65moonraker stop
-        /etc/init.d/S70httpd stop
-        /etc/init.d/S80guppyscreen stop
+        [ "$2" -eq 1 ] && /etc/init.d/S80guppyscreen stop
+        [ "$3" -eq 1 ] && /etc/init.d/S65moonraker stop
+        [ "$4" -eq 1 ] && /etc/init.d/S70httpd stop
     fi
 }
 
